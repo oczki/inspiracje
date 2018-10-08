f83a266f17d3c0f307d056c50a1b3dac03667d3a
fri <tehoko@gmail.com>
2018-10-09 00:40:51 +0200

    reordered sections

--- js/containers.js
+++ js/containers.js
@@ -3,7 +3,7 @@ let containers = [
     { type: "location",   color: "#14a020", label: "miejsce" },
     { type: "character",  color: "#ce9900", label: "postać" },
     { type: "character-modifier", color: "#e25b00", label: "cecha postaci" },
-    { type: "relation",   color: "#d40b0b", label: "kontekst" },
-    { type: "emotion",    color: "#bb2392", label: "emocja" },
+    { type: "emotion",    color: "#d40b0b", label: "emocja" },
+    { type: "relation",   color: "#bb2392", label: "kontekst" },
     { type: "dictionary", color: "#777777", label: "dowolne słowo" },
 ];
