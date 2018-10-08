49ea0b9a140d7442b244341ae72e3179af1ef355
fri <tehoko@gmail.com>
2018-10-09 00:09:08 +0200

    added relations list
    closes #3

--- js/containers.js
+++ js/containers.js
@@ -3,7 +3,7 @@ let containers = [
     { type: "location",   color: "#14a020", label: "miejsce" },
     { type: "character",  color: "#ce9900", label: "postać" },
     { type: "character-modifier", color: "#e25b00", label: "cecha postaci" },
-    { type: "relation",   color: "#d40b0b", label: "relacja" },
+    { type: "relation",   color: "#d40b0b", label: "kontekst" },
     { type: "emotion",    color: "#bb2392", label: "emocja" },
     { type: "dictionary", color: "#777777", label: "dowolne słowo" },
 ];
