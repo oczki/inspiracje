4966934542220a9de8ba15344deddf15146724b3
tehoko <tehoko@gmail.com>
2018-10-16 01:06:02 +0200

    cache busting for css and js
    closes #16

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
-<!DOCTYPE html>
+<?php
+function timestampUrl($path) {
+    return $path . '?' . filemtime($path);
+}
+
+function addCss($name) {
+    echo '<link rel="stylesheet" href="'
+         . timestampUrl('./css/' . $name . '.css') . '" type="text/css">'
+         . "\n";
+}
+
+function addJs($name) {
+    echo '<script src="' . timestampUrl('./js/' . $name . '.js') . '"></script>'
+         . "\n";
+}
+
+?><!DOCTYPE html>
 <html lang="pl-PL">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <meta name="viewport" content="initial-scale=1.0, width=device-width">
         <title>Magazyn inspiracji</title>
         <link rel="shortcut icon" type="image/png" href="favicon.png"/>
-        <link rel="stylesheet" href="./css/style.css" type="text/css">
-        <link rel="stylesheet" href="./css/media.css" type="text/css">
-        <script src="./js/containers.js"></script>
-        <script src="./js/core.js"></script>
-        <script src="./js/outline.js"></script>
+        <?php addCss('style'); ?>
+        <?php addCss('media'); ?>
+        <?php addJs('containers'); ?>
+        <?php addJs('core'); ?>
+        <?php addJs('outline'); ?>
     </head>
     <body>
         <main>
