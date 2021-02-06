afb98dfe4001dea17acc3bb0e34c98177deb380a
oko <tehoko@gmail.com>
2021-02-06 20:30:21 +0100

    capitalize group labels

--- js/containers.js
+++ js/containers.js
@@ -1,37 +1,37 @@
 let containers = [
   {
     type: "location",
-    label: "miejsce",
+    label: "Miejsce",
     color: "#14a020",
     icon: 'map-pin',
   },
   {
     type: "character",
-    label: "postać",
+    label: "Postać",
     color: "#ce9900",
     icon: 'user',
   },
   {
     type: "character-modifier",
-    label: "cecha postaci",
+    label: "Cecha postaci",
     color: "#e25b00",
     icon: 'user-plus',
   },
   {
     type: "emotion",
-    label: "emocja",
+    label: "Emocja",
     color: "#d40b0b",
     icon: 'heart',
   },
   {
     type: "relation",
-    label: "relacja",
+    label: "Relacja",
     color: "#bb2392",
     icon: 'hierarchy',
   },
   {
     type: "noun",
-    label: "rzeczownik",
+    label: "Rzeczownik",
     color: "#1c74c1",
     icon: 'box',
   },
