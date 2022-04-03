5e9d2a805df43af889b6647b5cd7294274c983d2
oko <tehoko@gmail.com>
2022-04-03 18:56:38 +0200

    add version comparison for data migration

--- js/core.js
+++ js/core.js
@@ -19,6 +19,8 @@ const iconEmail = '<svg viewBox="0 0 24 24"><path fill="currentColor" d="M20,8L1
 const iconGithub = '<svg viewBox="0 0 24 24"><path fill="currentColor" d="M12,2A10,10 0 0,0 2,12C2,16.42 4.87,20.17 8.84,21.5C9.34,21.58 9.5,21.27 9.5,21C9.5,20.77 9.5,20.14 9.5,19.31C6.73,19.91 6.14,17.97 6.14,17.97C5.68,16.81 5.03,16.5 5.03,16.5C4.12,15.88 5.1,15.9 5.1,15.9C6.1,15.97 6.63,16.93 6.63,16.93C7.5,18.45 8.97,18 9.54,17.76C9.63,17.11 9.89,16.67 10.17,16.42C7.95,16.17 5.62,15.31 5.62,11.5C5.62,10.39 6,9.5 6.65,8.79C6.55,8.54 6.2,7.5 6.75,6.15C6.75,6.15 7.59,5.88 9.5,7.17C10.29,6.95 11.15,6.84 12,6.84C12.85,6.84 13.71,6.95 14.5,7.17C16.41,5.88 17.25,6.15 17.25,6.15C17.8,7.5 17.45,8.54 17.35,8.79C18,9.5 18.38,10.39 18.38,11.5C18.38,15.32 16.04,16.16 13.81,16.41C14.17,16.72 14.5,17.33 14.5,18.26C14.5,19.6 14.5,20.68 14.5,21C14.5,21.27 14.66,21.59 15.17,21.5C19.14,20.16 22,16.42 22,12A10,10 0 0,0 12,2Z" /></svg>';
 const iconExternalLink = '<svg viewBox="0 0 24 24"><path fill="currentColor" d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" /></svg>';
 
+let version = '2.1';
+
 let containerColors = [
   {
     hue: 69,
@@ -182,6 +184,33 @@ let containerColors = [
       },
     },
   },
+  {
+    hue: 270,
+    lightMode: {
+      saturation: {
+        card: 61,
+        header: 88,
+        word: 60,
+      },
+      lightness: {
+        card: 93,
+        header: 36,
+        word: 38,
+      },
+    },
+    darkMode: {
+      saturation: {
+        card: 28,
+        header: 37,
+        word: 75,
+      },
+      lightness: {
+        card: 12,
+        header: 54,
+        word: 77,
+      },
+    },
+  },
 ]
 
 let containers = [
@@ -215,6 +244,11 @@ let containers = [
   {
     type: "action",
     label: "Czynność",
+    isVisible: true,
+  },
+  {
+    type: "object",
+    label: "Przedmiot",
     isVisible: true,
   },
 ];
@@ -554,10 +588,18 @@ class ColorSetter {
 
 let Categories = new function() {
   this.categoriesData = [];
-  this.keyName = 'categories-data';
+  this.categoriesKeyName = 'categories-data';
+  this.versionKeyName = 'app-version'; // Used to force update on user's side if new categories appear
 
   this.initialize = () => {
-    this.categoriesData = JSON.parse(localStorage.getItem(this.keyName)) || this.getOriginalData();
+    const localVersion = JSON.parse(localStorage.getItem(this.versionKeyName)) || 0.0;
+    if (localVersion === version) {
+      this.categoriesData = JSON.parse(localStorage.getItem(this.categoriesKeyName)) || this.getOriginalData();
+    } else {
+      this.categoriesData = this.getOriginalData();
+      localStorage.setItem(this.versionKeyName, JSON.stringify(version));
+    }
+    this.saveData();
   }
 
   this.forceReset = () => {
@@ -588,7 +630,7 @@ let Categories = new function() {
   }
 
   this.saveData = () => {
-    localStorage.setItem(this.keyName, JSON.stringify(this.categoriesData));
+    localStorage.setItem(this.categoriesKeyName, JSON.stringify(this.categoriesData));
   }
 
   this.getDataOfVisible = () => {
@@ -1305,17 +1347,21 @@ let SheetCreator = new function() {
   }
 
   this.createAboutSheet = () => {
-    const sheet = Creator.createSlidingSheet('about');
+    const sheetName = 'about';
+    const sheet = Creator.createSlidingSheet(sheetName);
     VisibilityController.preventTabbingToElement(sheet);
+
+    const rightSideContent = Creator.createElementWithClass('div', 'with-text');
+    rightSideContent.appendChild(Creator.createParagraph(`v${version}`, 'version-number'));
 
     const sheetContent = sheet.children[0];
 
-    sheetContent.appendChild(Creator.createSlidingSheetHeader('Magazyn Inspiracji'));
+    sheetContent.appendChild(Creator.createSlidingSheetHeader('Magazyn Inspiracji', rightSideContent));
 
-    sheetContent.appendChild(Creator.createParagraph('Generator słów do scen teatru improwizowanego.', 'sliding-sheet-text'));
-    sheetContent.appendChild(Creator.createParagraph('Na&nbsp;występach źródłem inspiracji jest publiczność, ' +
+    sheetContent.appendChild(Creator.createParagraph('Zbiór pomysłów do scen teatru impro&shy;wizo&shy;wanego.', 'sliding-sheet-text'));
+    sheetContent.appendChild(Creator.createParagraph('Na&nbsp;występach źródłem inspiracji jest publicz&shy;ność, ' +
       'lecz&nbsp;jej pomoc nie&nbsp;jest dostępna podczas prób i&nbsp;ćwiczeń. Magazyn Inspiracji wypełnia tę&nbsp;lukę ' +
-      'zbiorem ponad 1500 pomysłów, podzielonych na&nbsp;wygodne kategorie.', 'sliding-sheet-text'
+      'ponad dwoma tysiącami pomysłów, podzielonych na&nbsp;wygodne kategorie.', 'sliding-sheet-text'
     ));
 
     sheetContent.appendChild(Creator.createSeparator());
