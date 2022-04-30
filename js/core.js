139df1d55e536992d96c975c348a7f63cd7be25a
oko <tehoko@gmail.com>
2022-04-30 23:00:59 +0200

    try string constructor for regexp to fool safari

--- js/core.js
+++ js/core.js
@@ -1167,7 +1167,7 @@ let Creator = new function() {
 
   this.addNonBreakingSpaces = (text) => {
     try {
-      const patternWithLookbehind = /(?<![\wąćęłńóśźż])(a|i|o|u|w|z|\d+)\s(?!_)/gi; // Digits or a conjunction that's not part of previous word (e.g. "o" matches, but "hello" doesn't)
+      const patternWithLookbehind = new RegExp('(?<![\\wąćęłńóśźż])(a|i|o|u|w|z|\\d+)\\s(?!_)', 'gi'); // Digits or a conjunction that's not part of previous word (e.g. "o" matches, but "hello" doesn't)
       return text.replace(patternWithLookbehind, '$1&nbsp;');
     } catch (e) {
       const patternDumbedDown = /([^\wąćęłńóśźż])(a|i|o|u|w|z)\s(?!_)/gi; // It's 2022 and Safari still doesn't support lookbehinds...
